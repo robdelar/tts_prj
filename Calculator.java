16d5919fd2a31f63d8970048c7a5f9789f4fe80e
robdelar <44275659+robdelar@users.noreply.github.com>
2022-07-28 22:23:11 -0700

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+package package2;
+
+public class Calculator {
+
+	public int addNum(int num1, int num2) {
+		return num1+num2;
+	}
+	
+	public int subtract(int num1, int num2) {
+		return num1-num2;
+	}
+	
+	public int mult(int num1, int num2) {
+		return num1*num2;
+	}
+	
+	public int div(int num1, int num2) {
+		return num1/num2;
+	}
+	
+	public int square(int num1) {
+		return num1*num1;
+	}
+}
